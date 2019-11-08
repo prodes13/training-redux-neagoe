aeaacc573c1d972e8f4e0024b7b06a9bfc1a2a92
prodes13 <ciobotaru.florin@gmail.com>
2019-11-08 11:40:24 +0200

    added redux logger

--- src/index.js
+++ src/index.js
@@ -7,13 +7,18 @@ import * as serviceWorker from './serviceWorker';
 // REDUX
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import { increaseNum } from './redux/reducers';
+import { createLogger } from 'redux-logger';
+import { reducer } from './redux/reducers';
 
-const store = createStore(increaseNum);
-
+const store = createStore(reducer);
+const log = createLogger();
 ReactDOM.render(
     <Provider store={store}>
         <App />
     </Provider>, document.getElementById('root'));
 
 serviceWorker.unregister();
+
+////////////////////////////////////////////////////
+//  https://daveceddia.com/how-does-redux-work/   //
+////////////////////////////////////////////////////
